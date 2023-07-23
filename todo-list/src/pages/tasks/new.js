3df76e2d6d884c2bcb60facadc10080dba8d84eb
nicolasgonzalez <104945758+nicogz90@users.noreply.github.com>
2023-07-22 22:37:28 -0300

    add new Task

--- todo-list/src/pages/tasks/new.js
+++ todo-list/src/pages/tasks/new.js
@@ -1,9 +1,42 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import { Form, Grid, Button } from "semantic-ui-react";
 
 export default function TaskForm() {
-  function handleSubmit(e) {
+  const [newTask, setNewTask] = useState({ title: "", description: "" });
+  const [errors, setErrors] = useState({ title: "", description: "" });
+  const router = useRouter();
+
+  const validate = () => {
+    const errors = {};
+    if (!newTask.title) errors.title = "Title is required";
+    if (!newTask.description) errors.description = "Description is required";
+    return errors;
+  };
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitting");
+    let errors = validate();
+    if (Object.keys(errors).length) return setErrors(errors);
+
+    await createTask();
+    router.push("/");
+  }
+
+  const createTask = async () => {
+    try {
+      await fetch("http://localhost:3000/api/tasks", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newTask),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  function handleChange(e) {
+    setNewTask({ ...newTask, [e.target.name]: e.target.value });
   }
 
   return (
@@ -15,9 +48,30 @@ export default function TaskForm() {
     >
       <Grid.Row>
         <Grid.Column textAlign="center">
+          <h1>Create Task</h1>
           <Form onSubmit={handleSubmit}>
-            <Form.Input label="Title" placeholder="Title" />
-            <Form.TextArea label="Description" placeholder="Description" />
+            <Form.Input
+              label="Title"
+              placeholder="Title"
+              name="title"
+              onChange={handleChange}
+              error={
+                errors.title
+                  ? { content: "Please enter title", pointing: "below" }
+                  : null
+              }
+            />
+            <Form.TextArea
+              label="Description"
+              placeholder="Description"
+              name="description"
+              onChange={handleChange}
+              error={
+                errors.description
+                  ? { content: "Please enter description", pointing: "below" }
+                  : null
+              }
+            />
             <Button primary>Save</Button>
           </Form>
         </Grid.Column>
